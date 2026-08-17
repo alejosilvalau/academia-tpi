7dcd0351d5344503fe8208a8b28fe130ca7ba7c4
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-08-17 08:34:14 -0300

    feat: Agregadas validaciones de usuarios, formularios y más de claves

--- Utils/Validaciones.cs
+++ Utils/Validaciones.cs
@@ -20,9 +20,50 @@ namespace Utils
             return clave.Equals(conf);
         }
 
+        public static bool ValidarClave(string clave_usr, string txt_clave)
+        {
+            if (string.IsNullOrEmpty(clave_usr) || !clave_usr.Equals(txt_clave))
+            {
+                return false;
+            }
+            return true;
+        }
+
         public static bool ValidarRegexNombreyApellido(string texto)
         {
             return Regex.IsMatch(texto, @"^[A-Za-zÀ-ú]+([-' ][A-Za-zÀ-ú]+)*$");
         }
+
+        public static bool FormularioCompleto(List<string> txts)
+        {
+            bool rta = true;
+            txts.ForEach(t =>
+            {
+                if (string.IsNullOrEmpty(t))
+                {
+                    rta = false;
+                }
+            });
+            return rta;
+        }
+
+        public static bool UsuarioNoExiste(string nombreUsuario)
+        {
+            if (new UsuarioLogic().GetOneNombreUsuario(nombreUsuario) != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool UsuarioLengthValido(string nombreUsuario)
+        {
+            if (nombreUsuario.Length > 15)
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 }
