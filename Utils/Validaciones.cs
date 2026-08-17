3948375b0513011bbeb56a5c030e961000e9abff
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-08-17 08:29:47 -0300

    feat: Agregado proyecto de utilidades y validacion de emails

---
+++ Utils/Validaciones.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    internal class Validaciones
+    {
+        public static bool ValidarRegexEmail(string email)
+        {
+            return Regex.IsMatch(email, @"^[a-z0-9!#$%&'""*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'""*+/=?^_`{|}~-]+)*" +
+                    @"@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+        }
+    }
+}
