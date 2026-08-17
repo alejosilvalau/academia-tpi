caec325198be5e2973cf7b599d6bfe19738f8ada
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-08-17 08:31:44 -0300

    feat: Nuevas utilidades de validaciones de Clave, Nombre y Apellido

--- Utils/Validaciones.cs
+++ Utils/Validaciones.cs
@@ -9,5 +9,20 @@ namespace Utils
             return Regex.IsMatch(email, @"^[a-z0-9!#$%&'""*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'""*+/=?^_`{|}~-]+)*" +
                     @"@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
         }
+
+        public static bool ValidarRegexClave(string clave)
+        {
+            return Regex.IsMatch(clave, ".{4,50}");
+        }
+
+        public static bool ValidarClaveConfirmada(string clave, string conf)
+        {
+            return clave.Equals(conf);
+        }
+
+        public static bool ValidarRegexNombreyApellido(string texto)
+        {
+            return Regex.IsMatch(texto, @"^[A-Za-zÀ-ú]+([-' ][A-Za-zÀ-ú]+)*$");
+        }
     }
 }
