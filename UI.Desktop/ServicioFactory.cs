d07655ac3493ac59963fe0fe9c4dbdb781101041
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-09-19 21:36:06 -0300

    feat: Creación única de Usuario Context Desktop en UI.Desktop

--- UI.Desktop/ServicioFactory.cs
+++ UI.Desktop/ServicioFactory.cs
@@ -8,22 +8,22 @@ namespace UI.Desktop
     public static class ServicioFactory
     {
         private static IDbContextFactory<AcademiaContext>? _factory;
-        private static IUsuarioContexto CrearContexto() => new UsuarioContextoDesktop();
+        private static readonly IUsuarioContexto _contexto = new UsuarioContextoDesktop();
 
         public static void Initialize(IServiceProvider provider)
         {
             _factory = provider.GetRequiredService<IDbContextFactory<AcademiaContext>>();
         }
 
-        public static PersonaServicio Persona() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static UsuarioServicio Usuario() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static EspecialidadServicio Especialidad() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static PlanServicio Plan() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static MateriaServicio Materia() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static ComisionServicio Comision() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static CursoServicio Curso() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static DocenteCursoServicio DocenteCurso() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static InscripcionServicio Inscripcion() => new(_factory!.CreateDbContext(), CrearContexto());
-        public static ReporteServicio Reporte() => new(CrearContexto());
+        public static PersonaServicio Persona() => new(_factory!.CreateDbContext(), _contexto);
+        public static UsuarioServicio Usuario() => new(_factory!.CreateDbContext(), _contexto);
+        public static EspecialidadServicio Especialidad() => new(_factory!.CreateDbContext(), _contexto);
+        public static PlanServicio Plan() => new(_factory!.CreateDbContext(), _contexto);
+        public static MateriaServicio Materia() => new(_factory!.CreateDbContext(), _contexto);
+        public static ComisionServicio Comision() => new(_factory!.CreateDbContext(), _contexto);
+        public static CursoServicio Curso() => new(_factory!.CreateDbContext(), _contexto);
+        public static DocenteCursoServicio DocenteCurso() => new(_factory!.CreateDbContext(), _contexto);
+        public static InscripcionServicio Inscripcion() => new(_factory!.CreateDbContext(), _contexto);
+        public static ReporteServicio Reporte() => new(_contexto);
     }
 }
